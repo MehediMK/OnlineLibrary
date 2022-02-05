2ffb7ddcb293742a45beeabff0595707cab44ee8
Mehedi Khan <mehedikhan.cse@gmail.com>
2022-02-05 22:39:12 +0600

    Update settings.py

--- OnlineLibrary/settings.py
+++ OnlineLibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6370w_ac5)c+5_zbks%+4&%lo%bhq0gj6k81fbeb5lz3iji#&'
+SECRET_KEY = 'contact with me'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
